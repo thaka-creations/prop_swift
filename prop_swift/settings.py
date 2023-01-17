5042ad033d426cbef98ee250a75e6c8cf6d20ac5
abdulnelfrank <nelfrankaj@gmail.com>
2023-01-17 12:16:27 +0300

    feat: add user to app and migrations

--- prop_swift/settings.py
+++ prop_swift/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -112,7 +113,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+AUTH_USER_MODEL = 'users.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
