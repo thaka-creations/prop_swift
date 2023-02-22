751b4a22876cf92b82531ec14b23fb1589eedb41
abdulnelfrank <nelfrankaj@gmail.com>
2023-02-22 09:40:31 +0300

    allow all hosts

--- prop_swift/settings.py
+++ prop_swift/settings.py
@@ -29,7 +29,7 @@ TOKEN_SECRET_KEY = os.environ.get('TOKEN_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
