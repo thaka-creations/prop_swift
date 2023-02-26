9b0ee18f896cc48461e173ca4489a4d9dd5d6b81
abdulnelfrank <nelfrankaj@gmail.com>
2023-02-26 22:12:43 +0300

    crontab updates

--- prop_swift/cron.py
+++ prop_swift/cron.py
@@ -2,7 +2,7 @@ from shared_utils import notification_utils
 
 
 def my_scheduled_job():
-    recipient = "kaita@gmail.com"
+    recipient = "nelfrankaj@gmail.com"
     notification_utils.send_email(
         subject="Test email",
         message="This is a test email",
